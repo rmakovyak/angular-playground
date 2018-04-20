6c967400e4e975743b519f2b8c9609fb9938acc4
Roman Makovyak <roman.makovyak@relayr.io>
2018-04-20 10:27:58 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app-material.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppMaterialModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
