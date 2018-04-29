22101e5865bbc245fa51158a4808dacd0f9f8730
Roman Makovyak <romanmakovyak@MacBook-Pro.local>
2018-04-29 18:17:17 +0200

    row highlight

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -11,7 +11,8 @@ import {
   MatIconModule,
   MatProgressBarModule,
   MatSidenavModule,
-  MatMenuModule
+  MatMenuModule,
+  MatChipsModule
 } from '@angular/material';
 
 @NgModule({
@@ -27,7 +28,8 @@ import {
     MatIconModule,
     MatProgressBarModule,
     MatSidenavModule,
-    MatMenuModule
+    MatMenuModule,
+    MatChipsModule
   ],
   exports: [
     MatButtonModule,
@@ -41,7 +43,8 @@ import {
     MatIconModule,
     MatProgressBarModule,
     MatSidenavModule,
-    MatMenuModule
+    MatMenuModule,
+    MatChipsModule
   ]
 })
 export class AppMaterialModule {}
